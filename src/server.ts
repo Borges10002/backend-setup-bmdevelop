e3d983daa057490a2fd36de7ba497d9b8c161949
Diego Borges <32522338+Borges10002@users.noreply.github.com>
2023-01-28 07:53:05 -0400

    add

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
+
+const app = Fastify();
+
+app.register(cors);
+app.register(appRoutes);
+
+const port = process.env.PORT || 3333;
+
+app
+  .listen({
+    port,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log(`HTTP Server running port ${port} ! `);
+  });
